8e71da5e20c89a1025c672addf23df66b9eb8fbf
TeodorIliev06 <tedo2505@gmail.com>
2025-10-04 22:48:12 +0300

    styler: improve styling and effects for adding town feature

--- Git basics/exercise-resources/01-Towns-Resources/towns.js
+++ Git basics/exercise-resources/01-Towns-Resources/towns.js
@@ -17,8 +17,12 @@ function deleteTown() {
     }
   }
 
-  if (removed) $("#result").text(townName + " deleted.");
-  else $("#result").text(townName + " not found.");
+  if (removed) {
+	showMessage(townName + " deleted.");
+  }
+  else {
+	showMessage(townName + " not found.");
+  }
 }
 
 function addTown() {
@@ -30,20 +34,25 @@ function addTown() {
 }
 
 function shuffleTowns() {
-	let towns = $("#towns option").toArray();
+  let towns = $("#towns option").toArray();
 
-	$("#towns").empty();
-	shuffleArray(towns);
-	
-	$('#towns').append(towns);
-	$('#result').text("Towns shuffled.");
+  $("#towns").empty();
+  shuffleArray(towns);
 
-	function shuffleArray(array) { 
-		for (var i = array.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var oldElement = array[i];
-			array[i] = array[j];
-			array[j] = oldElement;
-		}
-	}
+  $("#towns").append(towns);
+  $("#result").text("Towns shuffled.");
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var oldElement = array[i];
+      array[i] = array[j];
+      array[j] = oldElement;
+    }
+  }
+}
+
+function showMessage(msg) {
+  $("#result").text(msg).css("display", "block");
+  setTimeout(() => $("#result").hide("blind", {}, 500), 3000);
 }

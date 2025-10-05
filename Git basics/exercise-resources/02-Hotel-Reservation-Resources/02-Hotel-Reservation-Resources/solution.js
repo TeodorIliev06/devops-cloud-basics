c25f1c93e76a6191354f2eca565161e0da91ec06
TeodorIliev06 <tedo2505@gmail.com>
2025-10-05 16:41:10 +0300

    feat: implement search functionality
    - validate check-in and check-out dates
    - hide/show components conditionally after successfull user interaction

--- Git basics/exercise-resources/02-Hotel-Reservation-Resources/02-Hotel-Reservation-Resources/solution.js
+++ Git basics/exercise-resources/02-Hotel-Reservation-Resources/02-Hotel-Reservation-Resources/solution.js
@@ -1,23 +1,95 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
+let reservation = {
+  startDate: null,
+  endDate: null,
+  guestsCount: 0,
+  roomType: null,
+  name: null,
+  phone: null,
+  email: null,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  changeContent("search-form-content");
+
+  document
+    .getElementById("search-form-btn")
+    .addEventListener("click", (e) => {
+		searchFormData(e);
+	});
+
+  function changeContent(className) {
+    document
+      .querySelectorAll(".custom-form")
+      .forEach((div) => div.classList.add("hidden"));
+    if (document.querySelector(`.${className}`) != null) {
+      document.querySelector(`.${className}`).classList.remove("hidden");
+    }
+  }
+
+  document
+    .querySelector("#new-reservation")
+    .addEventListener("click", (e) => cleanData(e));
+
+  function cleanData(e) {
+    changeContent("search-form-content");
+  }
+
+  function searchFormData(e) {
+    e.preventDefault();
+
+    const checkIn = document.getElementById("check-in").value;
+    const checkOut = document.getElementById("check-out").value;
+    const numberOfPeople = document.getElementById("people").value;
+
+    if (
+      checkIn != "" &&
+      checkOut != "" &&
+      numberOfPeople != "" &&
+      new Date(checkIn) <= new Date(checkOut)
+    ) {
+      reservation.startDate = checkIn;
+      reservation.endDate = checkOut;
+      reservation.guestsCount = numberOfPeople;
+
+      //changeContent("search-result-form-content");
+    }
+  }
+
+  /*
+document.querySelector('#guest-details-back-btn').addEventListener('click', (e) => fillRoomForm(e));
+
+function fillRoomForm(e) {
+    e.preventDefault();
+    changeContent('search-result-form-content');
 }
 
-function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
+document.querySelector('#guest-details-next-btn').addEventListener('click', (e) => getPersonalData(e));
+
+function getPersonalData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement.parentElement;
+
+    const name = data.querySelector('#name').value;
+    const phone = data.querySelector('#phone-number').value;
+    const email = data.querySelector('#email').value;
+
+    if (name != '' && phone != '' && email != '') {
+        reservation.name = name;
+        reservation.phone = phone;
+        reservation.email = email;
+        console.log(reservation);
+        changeContent('confirm-reservation-content');
+        fillConfirmReservationData(reservation);
     }
 }
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
-
-function cleanData(e) {
-    changeContent('search-form-content');
+function fillConfirmReservationData(customReservation) {
+    document.querySelector('.confirm-reservation #guest-name').textContent = `Name: ${customReservation.name}`;
+    document.querySelector('.confirm-reservation #guest-phone').textContent = `Phone Number: ${customReservation.phone}`;
+    document.querySelector('.confirm-reservation #guest-email').textContent = `Email: ${customReservation.email}`;
+    document.querySelector('.confirm-reservation #guest-room-type').textContent = `Room Type: ${customReservation.roomType}`;
+    document.querySelector('.confirm-reservation #guest-data-in').textContent = `Date-in: ${customReservation.startDate}`;
+    document.querySelector('.confirm-reservation #guest-data-out').textContent = `Date-out: ${customReservation.endDate}`;
 }
+*/
+});

6817002c7f15cff17c058778788218de10b23509
Bozhidara Partenova <141734012+TeodorIliev06@users.noreply.github.com>
2025-10-06 19:01:34 +0300

    fix: resolve merge conflicts

--- Git basics/exercise-resources/02-Hotel-Reservation-Resources/02-Hotel-Reservation-Resources/solution.js
+++ Git basics/exercise-resources/02-Hotel-Reservation-Resources/02-Hotel-Reservation-Resources/solution.js
@@ -1,23 +1,68 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
-}
+let reservation = {
+  startDate: null,
+  endDate: null,
+  guestsCount: 0,
+  roomType: null,
+  name: null,
+  phone: null,
+  email: null,
+};
 
-function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
+document.addEventListener("DOMContentLoaded", () => {
+  changeContent("search-form-content");
+
+  document.getElementById("search-form-btn").addEventListener("click", (e) => {
+    searchFormData(e);
+  });
+
+  document
+    .querySelector("#new-reservation")
+    .addEventListener("click", (e) => cleanData(e));
+
+  document
+    .querySelector("#search-back-btn")
+    .addEventListener("click", (e) => fillSearchForm(e));
+
+  document.querySelectorAll(".room-type").forEach((room) => {
+    room.addEventListener("click", (e) => selectRoomType(e));
+  });
+
+  document
+    .querySelector("#search-next-btn")
+    .addEventListener("click", (e) => findRoom(e));
+
+  function findRoom(e) {
+    e.preventDefault();
+
+    const roomInfo = document.querySelector(".selected-room h4").textContent;
+    reservation.roomType = roomInfo;
+
+    //changeContent("guest-details-form-content");
+  }
+
+  document
+    .querySelector("#guest-details-back-btn")
+    .addEventListener("click", (e) => fillRoomForm(e));
+
+  function fillRoomForm(e) {
+    e.preventDefault();
+    changeContent("search-result-form-content");
+  }
+
+  function changeContent(className) {
+    document
+      .querySelectorAll(".custom-form")
+      .forEach((div) => div.classList.add("hidden"));
+    if (document.querySelector(`.${className}`) != null) {
+      document.querySelector(`.${className}`).classList.remove("hidden");
     }
-}
+  }
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+  document
+    .querySelector("#new-reservation")
+    .addEventListener("click", (e) => cleanData(e));
 
-function cleanData(e) {
-    changeContent('search-form-content');
-}
+  function cleanData(e) {
+    changeContent("search-form-content");
+  }
+});

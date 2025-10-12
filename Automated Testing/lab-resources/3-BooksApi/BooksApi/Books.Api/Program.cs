ef28ad82d6133b4cf9ffc05dfc56f0e7c08d2e49
Bozhidara Partenova <141734012+TeodorIliev06@users.noreply.github.com>
2025-10-12 22:55:19 +0300

    feat: implement books api

---
+++ Automated Testing/lab-resources/3-BooksApi/BooksApi/Books.Api/Program.cs
@@ -0,0 +1,59 @@
+namespace Books.Api
+{
+    using System.Reflection;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.OpenApi.Models;
+
+    using Books.Core.Contracts;
+    using Books.Core.Services;
+    using Books.Infrastructure.Data.Contexts;
+    using Books.Infrastructure.Data.Repositories;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<BooksContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Books.API",
+                    Version = "v1"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            builder.Services.AddTransient<IBooksRepository, BooksRepository>();
+            builder.Services.AddTransient<IBookService, BookService>();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}

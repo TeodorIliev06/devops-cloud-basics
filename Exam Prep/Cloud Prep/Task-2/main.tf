159d7ed9070b11dbfe009fd1cfad0c464488d1df
Teodor Iliev <tedo2505@gmail.com>
2025-12-16 23:44:22 +0200

    feat: add sample main.tf file

--- Exam Prep/Cloud Prep/Task-2/main.tf
+++ Exam Prep/Cloud Prep/Task-2/main.tf
@@ -8,5 +8,84 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+  }
+
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = "WatchlistResourceGroup"
+  location = "Poland Central"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "WatchlistServicePlan"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "WatchlistLinuxWebApp"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT = "Development"
+  }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "watchlistsqlserverdemo"
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "database" {
+  name                 = "watchlistdb"
+  server_id            = azurerm_mssql_server.sqlserver.id
+  collation            = "SQL_Latin1_General_CP1_CI_AS"
+  license_type         = "LicenseIncluded"
+  max_size_gb          = 2
+  sku_name             = "S0"
+  zone_redundant       = false
+  geo_backup_enabled   = false
+  storage_account_type = "Local"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "example" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = "https://github.com/TeodorIliev06/devops-cloud-basics"
+  branch                 = "main"
+  use_manual_integration = true
 }

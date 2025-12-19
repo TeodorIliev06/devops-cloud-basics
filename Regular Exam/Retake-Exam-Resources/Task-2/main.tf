6fa8ceaceab1a249f89ed0dfb97d3d1138f4ef14
Teodor Iliev <tedo2505@gmail.com>
2025-12-19 12:18:47 +0200

    feat: add terraform config tiles

---
+++ Regular Exam/Retake-Exam-Resources/Task-2/main.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.56.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = var.web_app_name
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT = "Development"
+  }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = var.admin_login
+  administrator_login_password = var.admin_login_password
+}
+
+resource "azurerm_mssql_database" "database" {
+  name                 = var.sql_database_name
+  server_id            = azurerm_mssql_server.sqlserver.id
+  collation            = "SQL_Latin1_General_CP1_CI_AS"
+  license_type         = "LicenseIncluded"
+  max_size_gb          = 2
+  sku_name             = "S0"
+  zone_redundant       = false
+  geo_backup_enabled   = false
+  storage_account_type = "Local"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = var.firewall_name
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "example" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.github_repo_url
+  branch                 = "main"
+  use_manual_integration = true
+}
